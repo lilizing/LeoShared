bc2f4c93e44e500e1ab2940211a4c148893b1cd1
lli <lli@snailgame.net>
2017-04-07 15:44:12 +0800

    add framework:LeoCommon

---
+++ LeoCommon/LeoCommon/LeoCommon.h
@@ -0,0 +1,19 @@
+//
+//  LeoCommon.h
+//  LeoCommon
+//
+//  Created by 李理 on 2017/4/7.
+//  Copyright © 2017年 李理. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeoCommon.
+FOUNDATION_EXPORT double LeoCommonVersionNumber;
+
+//! Project version string for LeoCommon.
+FOUNDATION_EXPORT const unsigned char LeoCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeoCommon/PublicHeader.h>
+
+
