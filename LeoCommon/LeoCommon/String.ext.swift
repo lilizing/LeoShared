31caaf0ae8b250ec56ab9111d051768d2aaa7c05
lli <lli@snailgame.net>
2017-04-23 14:01:19 +0800

    extension for string +=

--- LeoCommon/LeoCommon/String.ext.swift
+++ LeoCommon/LeoCommon/String.ext.swift
@@ -12,7 +12,7 @@ import HEXColor
 import SwiftRichString
 
 extension String {
-    public func string(name:String, size:Int, hex:String) -> NSAttributedString {
+    public func string(name:String, size:Int, hex:String) -> NSMutableAttributedString {
         let style = Style.init { style in
             style.font = FontAttribute.init(name, size: Float(size))
             style.color = UIColor(hex)
@@ -20,11 +20,19 @@ extension String {
         return self.set(style: style)
     }
     
-    public func string(name:String, size:Int, color:UIColor) -> NSAttributedString {
+    public func string(name:String, size:Int, color:UIColor) -> NSMutableAttributedString {
         let style = Style.init { style in
             style.font = FontAttribute.init(name, size: Float(size))
             style.color = color
         }
         return self.set(style: style)
+    }
+}
+
+extension NSAttributedString {
+    static public func += (left: inout NSAttributedString, right: NSAttributedString) {
+        let ns = left is NSMutableAttributedString ? (left as! NSMutableAttributedString) : NSMutableAttributedString(attributedString: left)
+        ns.append(right)
+        left = ns
     }
 }
